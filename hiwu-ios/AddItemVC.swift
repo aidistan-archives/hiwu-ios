908a834387a838c57e0b6b086fa1ff4166e6d322
阮良 <ruanliangde123@163.com>
2015-10-30 13:26:28 +0800

    添加了城市列表，但是还不知道怎么用

--- hiwu-ios/AddItemVC.swift
+++ hiwu-ios/AddItemVC.swift
@@ -18,11 +18,10 @@ class AddItemVC: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,UI
 
     @IBAction func locationSelect(sender: UIButton) {
     }
-    @IBAction func ownerSelector(sender: UIButton) {
-    }
+
     @IBOutlet weak var time: UILabel!
     @IBOutlet weak var city: UILabel!
-    @IBOutlet weak var ownerAvatar: UIImageView!
+
     
     
     
@@ -40,12 +39,24 @@ class AddItemVC: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,UI
         
     }
     
+    @IBOutlet weak var museumName: UIButton!
     @IBAction func museumSelector(sender: UIButton) {
         
         let alert = UIAlertController(title: "选择博物馆", message: "\n\n\n\n\n\n\n\n\n", preferredStyle: UIAlertControllerStyle.ActionSheet)
         alert.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Default, handler: nil))
         alert.addAction(UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "添加博物馆", style: UIAlertActionStyle.Default, handler: nil))
+        alert.addAction(UIAlertAction(title: "添加博物馆", style: UIAlertActionStyle.Default, handler: {sender in
+            let alert1 = UIAlertController(title: "添加博物馆", message: "请输入博物馆的名字", preferredStyle: UIAlertControllerStyle.Alert)
+            alert1.addTextFieldWithConfigurationHandler(nil)
+            alert1.addAction(UIAlertAction(title: "取消", style: UIAlertActionStyle.Cancel, handler: nil))
+            alert1.addAction(UIAlertAction(title: "确定", style: UIAlertActionStyle.Default, handler: {(sender) in
+                self.museumName.setTitle(alert1.textFields![0].text, forState: UIControlState.Normal)
+                let x = globalHiwuUser.selfMuseum!["galleries"][0]
+                print(x)
+            }))
+            
+            self.navigationController?.presentViewController(alert1, animated: true, completion: nil)
+        }))
         let picker = UIPickerView(frame: CGRect(x: 30, y: 40, width: 300, height: 150));
         picker.backgroundColor = UIColor.brownColor()
         picker.delegate = self
@@ -124,7 +135,10 @@ class AddItemVC: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,UI
             
             var newFrame = self.view.frame
             newFrame.origin.y = offset
+            UIView.animateWithDuration(0.5, animations: {() in
             self.view.frame = newFrame
+            })
+            
         }
         if(textView.text == "在这里添加物品描述"){
             textView.text = ""
@@ -137,32 +151,41 @@ class AddItemVC: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,UI
             textView.text = "在这里添加物品描述"
         }
         textView.resignFirstResponder()
-        self.view.frame.origin.y = 0
+        UIView.animateWithDuration(0.1, animations: {void in
+            self.view.frame.origin.y = 0
+        })
+        
     }
     
     func textFieldDidBeginEditing(textField: UITextField){
         let location = textField.frame
         let size = self.view.frame
-        print(location)
-        print(size)
-        let offset = size.height - location.maxY - 300
+        let offset = size.height - location.maxY - 280
         if(offset<0){
-            
-            var newFrame = self.view.frame
-            newFrame.origin.y = offset
-            self.view.frame = newFrame
+            UIView.animateWithDuration(0.5, animations: {void in
+                var newFrame = self.view.frame
+                newFrame.origin.y = offset
+                self.view.frame = newFrame})
         }
 
     }
     
     func textFieldDidEndEditing(textField: UITextField){
         textField.resignFirstResponder()
-        self.view.frame.origin.y = 0
+        UIView.animateWithDuration(0, animations: {void in
+            self.view.frame.origin.y = 0
+        })
     }
     
     func endTheEditingOfTextView(){
         self.itemDescription.endEditing(true)
         self.itemName.endEditing(true)
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        print(row)
+        print(component)
+    
     }
 
     
