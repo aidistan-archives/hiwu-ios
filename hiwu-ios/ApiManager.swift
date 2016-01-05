a2acb36b4e6254a2c7ba46e0c8ce9c1088cf4409
阮良 <ruanliangde123@163.com>
2016-01-05 17:53:00 +0800

    更新了today的地址

--- hiwu-ios/ApiManager.swift
+++ hiwu-ios/ApiManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class ApiManager{
     static let simpleLogin = "http://hiwu.ren:3010/api/HiwuUsers/simpleLogin?"
-    static let getTodayPublicView = "http://hiwu.ren:3010/api/Today/publicView"
+    static let getTodayPublicView = "http://hiwu.ren:3010/api/SelectedGalleries/publicView"
     static let getAllSelfGallery1_2 = "http://hiwu.ren:3010/api/HiwuUsers/"
     static let getAllSelfGallery2_2 = "?filter=%7B%22include%22%3A%7B%22galleries%22%3A%7B%22items%22%3A%5B%22photos%22%2C%22comments%22%5D%7D%7D%7D&access_token="
     static let getSelfItem1 = "http://hiwu.ren:3010/api/Items/"
